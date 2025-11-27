8210a6ba4b1f0fd278cc28e5b256b71296de4db2
agung866 <setiawan.agung866@gmail.com>
2025-11-27 06:34:42 -0800

    Navbar

---
+++ src/app/Menu.tsx
@@ -0,0 +1,9 @@
+import '../index.css';
+
+export default function Menu() {
+    return (
+        <div className="container mx-auto flex m-3">
+
+        </div>
+    )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import Navbar from './app/Navbar.tsx'
 import Search from './app/Search.tsx'
 import './index.css'
 import SearchByCategory from "./app/SearchByCategory.tsx";
+import Menu from "./app/Menu.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <Navbar/>
         <Search/>
         <SearchByCategory/>
+        <Menu/>
     </StrictMode>,
 )
