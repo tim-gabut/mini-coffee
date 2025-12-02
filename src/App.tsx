ca181fee18a33e2716bd0db8266d2618dbc80986
agung866 <setiawan.agung866@gmail.com>
2025-12-02 06:04:05 -0800

    logic cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,50 @@
 import SearchBar from "./app/SearchBar.tsx";
-import Menu from "./app/Menu.tsx";
+import Menu, {type MenuItem} from "./app/Menu.tsx";
 import BottomNav from "./app/BottomNav.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 interface CartItem {
     id: number;
     menuName: string;
-    harga: number;
+    price: number;
     qty: number;
 }
 
-export default function App(){
-    const [count, setCount] = useState(0)
-    const countItem =()=>{
-        setCount(count+1)
+export default function App() {
+    const [cart, setCart] = useState<CartItem[]>([])
+
+    const handleAddToCart = (itemMenu: MenuItem) => {
+        const isFoundItem = cart.find(item => item.id === itemMenu.id);
+        if (isFoundItem) {
+
+            setCart(prev =>
+                prev.map(item =>
+                    item.id === itemMenu.id
+                        ? { ...item, qty: item.qty + 1 }
+                        : item
+                )
+            );
+        } else {
+            const newItem: CartItem = {
+                id: itemMenu.id,
+                menuName: itemMenu.menuName,
+                price: itemMenu.price,
+                qty: 1
+            };
+            setCart([...cart, newItem]);
+
+        }
     }
+    const totalQtyGlobal = cart.reduce((total, item) => total + item.qty, 0);
+    useEffect(() => {
+        console.log("Cart Updated:", cart);
+        console.log("Total Qty:", totalQtyGlobal);
+    }, [cart, totalQtyGlobal]);
     return (
         <>
             <SearchBar/>
-            <Menu funcAddItem={countItem}/>
-            <BottomNav totalItems={count}/>
+            <Menu funcAddItem={handleAddToCart}/>
+            <BottomNav totalItems={totalQtyGlobal}/>
         </>
     )
 }
--- src/app/Menu.tsx
+++ src/app/Menu.tsx
@@ -4,7 +4,7 @@ import {ShoppingCart} from "lucide-react";
 
 // import {useState} from "react";
 
-interface Menu {
+export interface MenuItem {
     id: number;
     menuName: string;
     price: number;
@@ -12,7 +12,7 @@ interface Menu {
     description: string;
 }
 
-const menus: Menu[] = [
+const menus: MenuItem[] = [
     {id:1,menuName: "Americano", price: 12000, image: images, description: "Kopi atau Makanan ini enak sekali"},
     {id:2,menuName: "Cappuccino", price: 20000, image: images, description: "Kopi atau Makanan ini enak sekali"},
     {id:3,menuName: "Espresso", price: 10000, image: images, description: "Kopi atau Makanan ini enak sekali"},
@@ -29,27 +29,27 @@ const menus: Menu[] = [
 ]
 
 interface MenuProps {
-    funcAddItem: () => void;
+    funcAddItem: (item: MenuItem) => void;
 }
 
 export default function Menu({funcAddItem}: MenuProps) {
 
     return (
         <div className="pt-16 pb-24 px-4 ">
-            <div className="bg-white rounded-2xl overflow-hidden
+            <div className="rounded-2xl overflow-hidden
             grid grid-cols-2 lg:grid-cols-4 gap-2">
                 {
-                    menus.map((menu: Menu, index) => (
-                        <div key={index} className="shadow-md rounded-2xl overflow-hidden mb-2
+                    menus.map((menu: MenuItem, index) => (
+                        <div key={index} className="bg-white shadow-md rounded-2xl overflow-hidden mb-2
                         transition:all hover:scale-98">
-                            <img src={menu.image} alt=""/>
+                            <img src={menu.image} alt="" className="w-full h-32 object-cover"/>
                             <div>
                                 <h3 className="font-bold text-lg w-full pl-2">{menu.menuName}</h3>
                                 <p className="text-sm pl-2">{menu.description}</p>
                                 <div className="flex justify-around justify-items-center mx-2 mb-2">
                                     <p className="flex flex-auto items-center">{menu.price}</p>
                                     <button
-                                        onClick={() => funcAddItem()}
+                                        onClick={() => funcAddItem(menu)}
                                         className=" text-amber-500 flex justify-center items-center
                                      border-amber-500 border rounded-md px-2 py-1
                                      transition-all hover:opacity-70 active:scale-90 active:bg-amber-600
